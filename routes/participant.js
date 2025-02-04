6368b4b970f5d10dac691f8ebd3f1338434e6702
rizvirishal1 <rizvirishal1@gmail.com>
2025-02-04 15:37:54 +0530

    up

--- routes/participant.js
+++ routes/participant.js
@@ -7,6 +7,7 @@ const participantRouter = express.Router();
 
 participantRouter.get("/:id", async (req, res) => {
     try {
+
         console.log("\n\n")
         console.log("GET participantRouter/:id is called...");
 
@@ -14,15 +15,18 @@ participantRouter.get("/:id", async (req, res) => {
         const startTime = new Date(process.env.START_TIME);
         const endTime = new Date(process.env.END_TIME);
 
-        if (currentTime < startTime) {
-            console.log("request returned a res mesaage: The event has not started yet");
-            console.log("\n\n")
-            return res.json({ message: "The event has not started yet" });
-        }
-        else if (currentTime > endTime) {
-            console.log("request returned a res mesaage: The event has ended");
-            console.log("\n\n")
-            return res.json({ message: "The event has ended" });
+        if (!req.params.id === process.env.ADMIN_ID) {
+
+            if (currentTime < startTime) {
+                console.log("request returned a res mesaage: The event has not started yet");
+                console.log("\n\n")
+                return res.json({ message: "The event has not started yet" });
+            }
+            else if (currentTime > endTime) {
+                console.log("request returned a res mesaage: The event has ended");
+                console.log("\n\n")
+                return res.json({ message: "The event has ended" });
+            }
         }
 
         const participant = await participantModel.findById(req.params.id);
--- routes/question.js
+++ routes/question.js
@@ -14,15 +14,17 @@ questionRouter.post("/:id", async (req, res) => {
         const startTime = new Date(process.env.START_TIME);
         const endTime = new Date(process.env.END_TIME);
 
-        if (currentTime < startTime) {
-            console.log("request returned a res mesaage: The event has not started yet");
-            console.log("\n\n")
-            return res.json({ message: "The event has not started yet" });
-        }
-        else if (currentTime > endTime) {
-            console.log("request returned a res mesaage: The event has ended");
-            console.log("\n\n")
-            return res.json({ message: "The event has ended" });
+        if (!req.params.id === process.env.ADMIN_ID) {
+            if (currentTime < startTime) {
+                console.log("request returned a res mesaage: The event has not started yet");
+                console.log("\n\n")
+                return res.json({ message: "The event has not started yet" });
+            }
+            else if (currentTime > endTime) {
+                console.log("request returned a res mesaage: The event has ended");
+                console.log("\n\n")
+                return res.json({ message: "The event has ended" });
+            }
         }
 
         const participant = await participantModel.findById(req.params.id);
