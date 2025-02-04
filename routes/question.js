f624bded5f694e75588109fde9da39ae04eff8c9
rizvirishal1 <rizvirishal1@gmail.com>
2025-02-04 18:34:16 +0530

    up

--- routes/question.js
+++ routes/question.js
@@ -14,17 +14,15 @@ questionRouter.post("/:id", async (req, res) => {
         const startTime = new Date(process.env.START_TIME);
         const endTime = new Date(process.env.END_TIME);
 
-        if (!req.params.id === process.env.ADMIN_ID) {
-            if (currentTime < startTime) {
-                console.log("request returned a res mesaage: The event has not started yet");
-                console.log("\n\n")
-                return res.json({ message: "The event has not started yet" });
-            }
-            else if (currentTime > endTime) {
-                console.log("request returned a res mesaage: The event has ended");
-                console.log("\n\n")
-                return res.json({ message: "The event has ended" });
-            }
+        if (currentTime < startTime) {
+            console.log("request returned a res mesaage: The event has not started yet");
+            console.log("\n\n")
+            return res.json({ message: "The event has not started yet" });
+        }
+        else if (currentTime > endTime) {
+            console.log("request returned a res mesaage: The event has ended");
+            console.log("\n\n")
+            return res.json({ message: "The event has ended" });
         }
 
         const participant = await participantModel.findById(req.params.id);
